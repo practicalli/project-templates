9a40ec7eeed76860fe33ffc4bf5b4fdb3b9adcb5
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-03-30 17:57:45 +0100

    root: src and test code
    Initially only basic example.  To be extended with an http service setup,
    optionally with Integrant

---
+++ resources/practicalli/template/service/test/clojure_app_template_test.clj
@@ -0,0 +1,7 @@
+(ns practicalli.clojure-app-template-test
+  (:require [clojure.test :refer :all]
+            [practicalli.clojure-app-template :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
