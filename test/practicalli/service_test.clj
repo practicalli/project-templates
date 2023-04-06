efa03ff54d8736050b4a923994da5d86eaf262c5
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-04-06 21:10:11 +0100

    dev: extract template.edn reader in unit test for service template

--- test/practicalli/service_test.clj
+++ test/practicalli/service_test.clj
@@ -1,3 +1,11 @@
+;; ---------------------------------------------------------
+;; Unit and specification tests for practicalli/service template
+;;
+;; Test the template.edn file of the application template
+;; using the deps-new specification
+;; ---------------------------------------------------------
+
+
 (ns practicalli.service-test
   (:require [clojure.edn :as edn]
             [clojure.java.io :as io]
@@ -6,8 +14,13 @@
             [org.corfield.new] ; deps-new Specs
             #_[practicalli.service :as service]))
 
+
+(def project-template
+  "Project template hash-map"
+  (edn/read-string (slurp (io/resource "practicalli/service/template.edn"))))
+
 (deftest valid-template-test
   (testing "template.edn is valid."
-    (let [template (edn/read-string (slurp (io/resource "practicalli/service/template.edn")))]
-      (is (spec/valid? :org.corfield.new/template template)
-          (spec/explain-str :org.corfield.new/template template)))))
+    (is (spec/valid? :org.corfield.new/template project-template)
+        (spec/explain-str :org.corfield.new/template project-template))))
+
