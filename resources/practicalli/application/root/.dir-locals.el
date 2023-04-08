cfc15c1e5b011bd3d2b62d48923ac917653c5262
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-04-08 14:32:52 +0100

    root: add :test/env alias for Cider jack-in support

--- resources/practicalli/application/root/.dir-locals.el
+++ resources/practicalli/application/root/.dir-locals.el
@@ -1,2 +1,2 @@
 ((clojure-mode . ((cider-preferred-build-tool . clojure-cli)
-                  (cider-clojure-cli-aliases . ":build:dev/reloaded"))))
+                  (cider-clojure-cli-aliases . ":build:test/env:dev/reloaded"))))
