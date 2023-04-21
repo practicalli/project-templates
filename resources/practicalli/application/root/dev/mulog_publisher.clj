3838e563613b0d4bcd186ebd0d60f833dc56f85a
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-04-21 12:56:29 +0100

    application: format mulog-publisher deftype code

--- resources/practicalli/application/root/dev/mulog_publisher.clj
+++ resources/practicalli/application/root/dev/mulog_publisher.clj
@@ -9,16 +9,17 @@
   (:require
    [com.brunobonacci.mulog.buffer :as mulog-buffer]))
 
-(deftype TapPublisher [buffer transform]
+(deftype TapPublisher
+  [buffer transform]
   com.brunobonacci.mulog.publisher.PPublisher
   (agent-buffer [_] buffer)
-
   (publish-delay [_] 200)
-
-  (publish [_ buffer]
+  (publish
+    [_ buffer]
     (doseq [item (transform (map second (mulog-buffer/items buffer)))]
       (tap> item))
     (mulog-buffer/clear buffer)))
+
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn tap
