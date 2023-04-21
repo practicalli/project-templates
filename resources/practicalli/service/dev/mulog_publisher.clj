d1a5a402c0fc774a93e2c14bdbd9106d391f4f0d
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-04-21 12:53:38 +0100

    dev: make system.clj a template
    Move dev files from root to their own directory and define declarative copy
    rules in transform.edn
    
    Use .template file extension to avoid false positives from cljstyle format check

--- resources/practicalli/service/dev/mulog_publisher.clj
+++ resources/practicalli/service/dev/mulog_publisher.clj
@@ -9,12 +9,11 @@
   (:require
    [com.brunobonacci.mulog.buffer :as mulog-buffer]))
 
-(deftype TapPublisher [buffer transform]
+(deftype TapPublisher
+  [buffer transform]
   com.brunobonacci.mulog.publisher.PPublisher
   (agent-buffer [_] buffer)
-
   (publish-delay [_] 200)
-
   (publish [_ buffer]
     (doseq [item (transform (map second (mulog-buffer/items buffer)))]
       (tap> item))
