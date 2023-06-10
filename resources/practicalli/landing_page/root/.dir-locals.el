2271d95d2fc52ff95eae0d85076ccd2965cb0f1a
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-06-10 21:32:35 +0100

    landing-page: :figwheel/env alias

--- resources/practicalli/landing_page/root/.dir-locals.el
+++ resources/practicalli/landing_page/root/.dir-locals.el
@@ -1,5 +1,5 @@
 ((clojurescript-mode . ((cider-preferred-build-tool          . clojure-cli)
-                        (cider-clojure-cli-aliases           . ":test/env:figwheel/env")
+                        (cider-clojure-cli-aliases           . ":test/env:figwheel/env:dev/reloaded-cljs")
                         (cider-default-cljs-repl             . figwheel-main)
                         (cider-figwheel-main-default-options . "dev")
                         (cider-repl-display-help-banner      . nil))))
