9384b0dd43ebffea208deb8cc7cc270f615880a3
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-06-12 22:04:05 +0100

    minimal: template programatic transformation

---
+++ src/practicalli/minimal.clj
@@ -0,0 +1,42 @@
+;; ---------------------------------------------------------
+;; Programatic Transformation
+;;
+;; Define transformations based on data in the template and
+;; passed via the command line
+;;
+;; `data-fn` enrich template data
+;; `template-fn` modify declarative template rules
+;; ---------------------------------------------------------
+
+
+(ns practicalli.minimal
+  "Programmatic transformation of template substitution data
+  and declarative transformation rules")
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn substitutions
+  "Update keys & values available in the substitution data,
+  from the template.edn, derived values and command line options
+  Return:
+  - hash-map to be merged into the existing substitution data"
+  [data]
+
+  ;; Simple example:
+  #_(when (= (data :specification) "clojure.spec")
+      {:clojure-spec true})
+
+  nil) ; returning nil means no changes to options data
+
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn template-edn
+  "Update data in the template.edn configuration.
+  E.g. derive new declarative transformation rules from
+  template substitutions, derived values and values passed
+  to the template via the command line
+  Return
+  - new template.edn configuration"
+  [edn data]
+
+  ;; Link to Practicalli Clojure Project Templates guide
+  (println "Template guide: https://practical.li/clojure/clojure-cli/projects/templates/")
+  edn)
---
+++ test/practicalli/minimal_test.clj
@@ -0,0 +1,23 @@
+;; ---------------------------------------------------------
+;; Unit and specification tests for practicalli/application template
+;;
+;; Test the template.edn file of the application template
+;; using the deps-new specification
+;; ---------------------------------------------------------
+
+
+(ns practicalli.minimal-test
+  (:require
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [clojure.spec.alpha :as spec]
+   [clojure.test :refer [deftest is testing]]
+   [org.corfield.new]
+   ;; deps-new Specs
+   #_[practicalli.application :as application]))
+
+(deftest valid-template-test
+  (testing "template.edn is valid."
+    (let [template (edn/read-string (slurp (io/resource "practicalli/application/template.edn")))]
+      (is (spec/valid? :org.corfield.new/template template)
+          (spec/explain-str :org.corfield.new/template template)))))
